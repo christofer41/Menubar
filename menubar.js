08c0e039587733bae0c97eba2d6c2b38e69a5d9e
Christofer Carlsson <christofer.carlsson@medieinstitutet.se>
2019-10-29 08:59:11 +0100

    Done

--- menubar.js
+++ menubar.js
@@ -1,10 +1,25 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
+function hamburgerMenu() {
     var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
+    var y = document.getElementById("myText");
+
+    if (x.style.display === "flex") {
       x.style.display = "none";
+      y.style.display = "block";
     } else {
-      x.style.display = "block";
+      x.style.display = "flex";
+      y.style.display = "none";
     }
-  }
+}
   
+let x = 16;
+function resizeText(){ 
+    x++;
+    const pTags = document.getElementsByTagName("p");
+
+    for (let i = 0; i <= pTags.length; i++) {
+
+    pTags[i].style.fontSize = x + "px";
+
+    }
+
+}
